97a49bd9ca57406f4c536c955145fa23e97d89a9
Gerard <gkhynes@gmail.com>
2017-10-31 21:05:21 +0000

    Initial commit

---
+++ assets/scripts.js
@@ -0,0 +1,24 @@
+const divs = document.querySelectorAll("div");
+const button = document.querySelectorAll("button");
+
+function logText(e) {
+  console.log(this.classList.value);
+  //   e.stopPropagation(); // stop bubbling
+}
+
+divs.forEach(div =>
+  div.addEventListener("click", logText, {
+    capture: false,
+    once: true // listens for a click once and then unbinds itself
+  })
+);
+
+button.addEventListener(
+  "click",
+  () => {
+    console.log("Click!");
+  },
+  {
+    once: true
+  }
+);
